d7d1f9c6b1861071910a94007b396c16c584ab44
seito2 <seito1201@gmial.com>
2019-01-22 21:31:29 +0900

    change the path

--- js/component/01_mybachground.js
+++ js/component/01_mybachground.js
@@ -1,8 +1,8 @@
 Vue.component('background',{
     template:
     '<div>'+
-        '<img class="pc" src="../assets/telstar_all.jpg">' +
-        '<img class="sp" src="../assets/telstar_all_copy.jpg">' +
+        '<img class="pc" src="./js/assets/telstar_all.jpg">' +
+        '<img class="sp" src="./js/assets/telstar_all_copy.jpg">' +
     '</div>'
 })
 
