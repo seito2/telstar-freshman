70c5545dc743eb830c71b79565bfd53358f0270d
seito2 <seito1201@gmial.com>
2019-01-23 00:35:04 +0900

    add contact.html

--- js/component/00_myheaders.js
+++ js/component/00_myheaders.js
@@ -1,9 +1,9 @@
 Vue.component('MyHeader',{
     template: 
     '<header><div class="logo">LOGO</div><nav class="active"><ul>' +
-        '<li><a href="contact.html" class="active">HOME</a></li>' +
+        '<li><a href="#" class="active">HOME</a></li>' +
         '<li><a href="#">PORTFOLIO</a></li>' +
-        '<li><a href="#">CONTACT</a></li>' +
+        '<li><a href="contact.html">CONTACT</a></li>' +
         '<li><a href="#">OTHERS</a></li>' +
     '</ul></nav><a class="square_btn" v-on:click="toggle">{{ title }}</a></header>'
     ,
